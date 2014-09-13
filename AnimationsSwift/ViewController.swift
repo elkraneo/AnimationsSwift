185a6354918f30e0f5b43d6a25bfd143845023a5
metodowhite <metodowhite@elkraneo-MacBook-Pro.local>
2014-09-13 13:45:04 +0200

    primera version

---
+++ AnimationsSwift/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AnimationsSwift
+//
+//  Created by metodowhite on 13/09/14.
+//  Copyright (c) 2014 metodowhite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //refactor
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
